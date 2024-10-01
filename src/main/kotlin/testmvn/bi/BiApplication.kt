72d66a71d4a73d0fbf798fb10c018606ecabf255
VLU-DEV <vlu-dev@email.com>
2024-10-01 21:05:15 +0700

    Init project

---
+++ src/main/kotlin/testmvn/bi/BiApplication.kt
@@ -0,0 +1,11 @@
+package testmvn.bi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BiApplication
+
+fun main(args: Array<String>) {
+    runApplication<BiApplication>(*args)
+}
---
+++ src/test/kotlin/testmvn/bi/BiApplicationTests.kt
@@ -0,0 +1,13 @@
+package testmvn.bi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
